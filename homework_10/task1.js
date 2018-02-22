49a16c8f7cb34a1828163c42ed5afb45e11db0c6
Bohdan Halamai <32569020+ImPaladin@users.noreply.github.com>
2018-02-23 01:33:13 +0200

    add homework_10

---
+++ homework_10/task1.js
@@ -0,0 +1,36 @@
+/**
+ * Your debounce function goes here
+ * function(){}
+ */
+function debounce(increaseIteratorBy1, delay) {
+    let timeout;
+    return () => {
+        clearTimeout(timeout);
+        timeout = setTimeout(increaseIteratorBy1, delay);
+    }
+}
+// Example
+
+let iterator = 0;
+
+function increaseIteratorBy1() {
+    iterator++;
+
+    printIteratorValue();
+}
+
+function printIteratorValue() {
+    console.log('Iterator value ', iterator);
+}
+
+var increaseIterator = debounce(increaseIteratorBy1, 5000);
+
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
